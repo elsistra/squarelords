4225edc318a29e50cc9ff68b9a6bef2fd171daa2
GarathSworn <garath@anon.systems>
2018-03-11 04:12:36 -0400

    Remove registration link if logged in

--- routes/index.js
+++ routes/index.js
@@ -7,26 +7,31 @@ const ObjectId = require("mongodb").ObjectId;
 
 module.exports = async (req, res, db) => {
 
-  // Read Cookie information to determine if user is logged in
-  const cookie = parseCookie(req);
-  let user;
-  // If the user has a session
-  if(cookie.session){
-    const session = await db.collection("sessions").findOne({_id: new ObjectId(cookie.session)});
-    // If a session has been found based pm the cookies information
-    if(session && session.userId){
-      // Find the user row from the users table which contains the userID found in sessions
-      user = await db.collection("users").findOne({_id: session.userId});
-    }
-  }
-  // Modify the index for logged in users
-  if(user.username){
-    document.getElementById('a-register').style.display = 'none';
-  }
-
-
   if(req.url == '/'){
-    const html = await readFile("views/index.html");
+    // Read Cookie information to determine if user is logged in
+    const cookie = parseCookie(req);
+    let user;
+    // If the user has a session
+    if(cookie.session){
+      const session = await db.collection("sessions").findOne({_id: new ObjectId(cookie.session)});
+      // If a session has been found based pm the cookies information
+      if(session && session.userId){
+        // Find the user row from the users table which contains the userID found in sessions
+        user = await db.collection("users").findOne({_id: session.userId});
+      }
+    }
+
+    let html = await readFile("views/index.html");
+
+    // Modify the index for logged in users
+    if(user){
+      html += `
+        <script>
+          document.getElementById('a-register').style.display = 'none';
+        </script>
+      `;
+    }
+
     res.end(html);
     return true; // Exit Function
   }
