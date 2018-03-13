26a694dc1a1b03b6d9c2503e8f8e1350965d2cab
elsistra <el@anon.systems>
2018-03-13 11:55:26 -0500

    Got Logout working.

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,18 @@ process.on("unhandledRejection", (error) => { throw error; });
 const fs = require("fs");
 const util = require("util");
 const readFile = util.promisify(fs.readFile);
-const parseCookie = require("../lib/parseCookie")
+const parseCookie = require("../lib/parseCookie");
+const checkForSession = require("../lib/checkForSession");
 const ObjectId = require("mongodb").ObjectId;
 
 module.exports = async (req, res, db) => {
 
   if(req.url == '/'){
-    // Read Cookie information to determine if user is logged in
-    const cookie = parseCookie(req);
-    let user;
-    // If the user has a session
-    if(cookie.session){
-      const session = await db.collection("sessions").findOne({_id: new ObjectId(cookie.session)});
-      // If a session has been found based pm the cookies information
-      if(session && session.userId){
-        // Find the user row from the users table which contains the userID found in sessions
-        user = await db.collection("users").findOne({_id: session.userId});
-      }
-    }
+    const sessionUser = await checkForSession(req, db);
 
     // Modify the index for logged in users
     let html = await readFile("views/index.html");
-    if(user){
+    if(sessionUser){
       html += `
         <script>
           document.getElementById('a-register').style.display = 'none';
--- routes/new-game.js
+++ routes/new-game.js
@@ -1,14 +1,23 @@
 const fs = require("fs");
-const util = require("util");
-const readFile = util.promisify(fs.readFile);
-const parseBody = require("../lib/parseBody")
+const parseCookie = require("../lib/parseCookie");
+const checkForSession = require("../lib/checkForSession");
 
 module.exports = async (req, res, db) => {
   if(req.url == '/new-game.html'){
-    console.log('New Game created');
+    // The user has signaled they wish to create a new game
+    // Make sure user is not already in a game
+    const sessionUser = await checkForSession(req, db);
+    if(sessionUser)new Promise(function(resolve, reject) {
+      console.log('User is signed in: ' + sessionUser.username);
+      // Create new Game
+      // Join user to game room
+
+      // Later index.js can be updated to list games
+    });
+    console.log('New Game created for ' + sessionUser.username);
     res.setHeader("Location", "/");
     res.statusCode = 302;
-    res.end("Game Created.");
+    res.end();
     return true; // Exit Function
   }
 }
