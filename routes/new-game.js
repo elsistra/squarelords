deecb44968c5e30555a45009b8406d75fe23e45b
elsistra <el@anon.systems>
2018-03-15 16:37:00 -0500

    Progress on New Game function

--- routes/new-game.js
+++ routes/new-game.js
@@ -7,11 +7,16 @@ module.exports = async (req, res, db) => {
     // The user has signaled they wish to create a new game
     // Make sure user is not already in a game
     const sessionUser = await checkForSession(req, db);
-    if(sessionUser)new Promise(function(resolve, reject) {
+    if(sessionUser)new Promise(async function(resolve, reject) {
       console.log('User is signed in: ' + sessionUser.username);
       // Create new Game
+      const newGame = {creatorId: sessionUser._id, status: 'open'};
+      const insertResult = await db.collection("games").insertOne(newGame);
       // Join user to game room
-
+      const filter = {_id: sessionUser._id};
+      const update = {$set: {gameId: insertResult.insertedId}};
+      await db.collection("users").updateOne(filter, update);
+      console.log('New Game created');
       // Later index.js can be updated to list games
     });
     console.log('New Game created for ' + sessionUser.username);
